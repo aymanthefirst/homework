57ba7fb92fcdcef39e96d8c870e22aee258af8b5
aymanthefirst <AHarake@spartaglobal.com>
2018-09-08 15:52:40 +0100

    Website

---
+++ ruby1/giraffe.rb
@@ -0,0 +1,26 @@
+# puts "Hello world"
+# anArray = ['1','2','3','4','5']
+# puts anArray
+# puts anArray.max
+#
+# i = 0
+# while i < anArray.length
+# item = anArray[i]
+# puts "current item: #{item}"
+#   i+=1
+# end
+
+# x = 5
+# if x > 2
+#   puts "x is greater than 2"
+# elsif x <= 2 and x !=0
+#   puts "x is 1"
+# else
+#   puts "i can't guess the number"
+# end
+maxNum = 0
+anArray = ['1','2','3','4','5']
+i = 0
+whie i > anArray.length
+if anArray[i] > maxNum
+  
