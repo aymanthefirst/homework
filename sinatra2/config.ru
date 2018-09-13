ab4e1b2cc628dfcdc1561e61f1515ecbf5225558
aymanthefirst <AHarake@spartaglobal.com>
2018-09-13 07:48:44 +0100

    sinatra

---
+++ sinatra2/config.ru
@@ -0,0 +1,5 @@
+require "sinatra"
+
+require_relative "./controllers/app.rb"
+
+run HiSinatra
---
+++ sinatra2/controllers/app.rb
@@ -0,0 +1,32 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+
+class HiSinatra < Sinatra::Base
+
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+
+  $books = [
+  {id: 0,
+  title: "p1",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {id: 1,
+  title: "p2",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  },
+  {id: 2,
+  title: "p3",
+  description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  }
+]
+
+  get "/books/:id" do
+    id = params[:id].to_i
+      @book = $books[id]
+      erb :'books'
+  end
+
+end
